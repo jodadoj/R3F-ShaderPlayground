4c55aa4ff2f2a2b7b8d1f3e8179d9394ed9beb9a
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-02-26 20:27:35 +0000

    z-axis, heading and subheading, new plane

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,68 @@
 import { OrbitControls } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, Vector3 } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
-import * as THREE from 'three';
+import * as THREE from "three";
 
 import vertexShader from "./vertexShader";
 import fragmentShader from "./fragmentShader";
 
 import "./App.css";
 
+interface IFlagProps{
+  rotate: number
+  wireframeOn: boolean
+  setPosition: Vector3
+}
 
-export function Flag():JSX.Element {
+export function Flag(props: IFlagProps): JSX.Element {
+  const rotate: number = props.rotate;
+  const wireframeOn = props.wireframeOn;
+  const setPosition = props.setPosition;
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<THREE.Mesh>(new THREE.Mesh());
 
-    // This reference will give us direct access to the mesh
-    const mesh = useRef<THREE.Mesh>(new THREE.Mesh());
-
-    useFrame(({ clock }) => {
-      const a = clock.getElapsedTime();
-      // mesh.current.rotation.y = a;
-    });
-
-    return (
-      <mesh
-        ref={mesh}
-        position={[0, 0, 0]}
-        // rotation={[-Math.PI / 2, 0, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        scale={1.5}
-      >
-        <planeGeometry args={[1, 1, 32, 32]} />
-        <shaderMaterial
-          fragmentShader={fragmentShader}
-          vertexShader={vertexShader}
-          wireframe
-        />
-      </mesh>
-    );
-  };
-
-export default function App(): JSX.Element {
-  
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    mesh.current.rotation.x = a / 2;
+    // mesh.current.rotation.y = a;
+    mesh.current.rotation.z = a;
+  });
 
   return (
+    <mesh
+      ref={mesh}
+      position={setPosition}
+      // rotation={[-Math.PI / 2, 0, 0]}
+      rotation={[rotate / 2, 0, 0]}
+      scale={1.5}
+    >
+      <planeGeometry args={[10, 10, 32, 32]} />
+      {wireframeOn ? 
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        wireframe
+      /> : <shaderMaterial
+      fragmentShader={fragmentShader}
+      vertexShader={vertexShader}
+    />}
+    </mesh>
+  );
+}
+
+export default function App(): JSX.Element {
+  return (
     <div className="ctn-fullscreen">
-      <Canvas camera={{ position: [1.0, 1.0, 1.0] }}>
-        <Flag />
-        <axesHelper />
+      <div className="ctn-headings">
+        <h1>Header</h1>
+        <h2>Subheading</h2>
+      </div>
+      <Canvas camera={{ position: [1.0, 0.0, 1.0] }}>
+        <Flag setPosition={[0, 3, 0]} wireframeOn={false} rotate={0} />
+        <Flag setPosition={[0, 0, 0]} wireframeOn={true} rotate={-Math.PI} />
+        <Flag setPosition={[0, 0, 0]} wireframeOn={true} rotate={Math.PI / 2} />
+        <Flag setPosition={[0, 0, 0]} wireframeOn={true} rotate={0} />
+        {/* <axesHelper /> */}
         <OrbitControls />
       </Canvas>
     </div>
